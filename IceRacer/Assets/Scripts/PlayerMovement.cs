f24d600673acfc584fad97b70713b49274a7abee
Kyle Doerksen <Kyledoerksen75@yahoo.ca>
2024-02-17 09:00:39 -0600

    Updated player movement

--- IceRacer/Assets/Scripts/PlayerMovement.cs
+++ IceRacer/Assets/Scripts/PlayerMovement.cs
@@ -5,7 +5,11 @@ using UnityEngine;
 public class PlayerMovement : MonoBehaviour
 {
     public float speed;
+    private float moveX;
+    private float moveY;
 
+    public float KMpH;
+    public float MaxKMpH;
 
     // Start is called before the first frame update
     void Start()
@@ -13,16 +17,41 @@ public class PlayerMovement : MonoBehaviour
         
     }
 
+    void FixedUpdate()
+    {
+        Vector3 movement = new Vector3(0, moveY, 0).normalized;
+        transform.position += movement * speed * Time.deltaTime;
+    }
+
     // Update is called once per frame
     void Update()
     {
-        float moveX = Input.GetAxisRaw("Horizontal");
-        float moveY = Input.GetAxisRaw("Vertical");
+        moveX = Input.GetAxisRaw("Horizontal");
+        if(KMpH < MaxKMpH && KMpH >= 0)
+        {
+            KMpH += moveX * speed * Time.deltaTime;
+        }
 
-        Vector3 movement = new Vector3(moveX, moveY, 0);
-        transform.position += movement * speed * Time.deltaTime;
-        Debug.Log(transform.position);
+        if(moveX == 0)
+        {
+            if(KMpH > 0)
+            {
+                KMpH -= speed/2 * Time.deltaTime;
+            }
+            else
+            {
+                KMpH = 0;
+            }
+        }
 
-        //transform.position = new Vector3(Mathf.RoundToInt(transform.position.x), Mathf.RoundToInt(transform.position.y), 0);
+        moveY = Input.GetAxisRaw("Vertical");
+        if(this.transform.position.y <= -19.02722f)
+        {
+            this.transform.position = new Vector3(this.transform.position.x, -19.02722f, 0);
+        }
+        if(this.transform.position.y >= 18.97278f)
+        {
+            this.transform.position = new Vector3(this.transform.position.x, 18.97278f, 0);
+        }
     }
 }
